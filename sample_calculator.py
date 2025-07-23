4bf46295fdcb433f704fafab09e80160e5d1158b
vpf-data <vivekpf.bi@gmail.com>
2025-07-23 11:48:06 +0530

    converted sunstraction  as a function

--- sample_calculator.py
+++ sample_calculator.py
@@ -1,14 +1,22 @@
 print('Hello there')
 
 print('--------------')
+x = int(input("enter first number : "))
+
+y = int(input("enter second number : "))
 
 
 def addition(x,y):
-    x= int(input("enter first number : "))
-
-    y=int(input("enter second number : "))
 
     z= x+y
     print(z)
 
-addition(3,5)
+
+
+def substraction(x,y):
+
+    z= x-y
+    print(z)
+
+addition(x,y)
+substraction(x,y)
